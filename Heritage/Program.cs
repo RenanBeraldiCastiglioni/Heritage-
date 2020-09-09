0c36178a4133efce99d17e935d9063f9b58e0729
Renan Beraldi <beraldi2019renan@gmail.com>
2020-09-09 15:50:40 -0300

    Project Ended

--- Heritage/Program.cs
+++ Heritage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Heritage.Entities;
 
 namespace Heritage
 {
@@ -6,7 +7,9 @@ namespace Heritage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account);
         }
     }
 }
